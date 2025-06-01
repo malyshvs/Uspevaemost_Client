6b09465a21fcfb96e3e2fa309b659fe50064f34b
ulyanov.nd <ulyanov.nd@W-017-044>
2025-06-02 08:54:43 +0900

    Добавьте файлы проекта.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace Uspevaemost_client.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<IndexModel> _logger;
+        public string? Username { get; private set; }
+
+        public void OnGet()
+        {
+            Username = User.Identity?.Name; // ← тут будет DOMAIN\username
+        }
+        public IndexModel(IHttpClientFactory httpClientFactory, ILogger<IndexModel> logger)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public List<string>? Kurs { get; set; }
+        [BindProperty]
+        public List<string>? Urovni { get; set; }
+        [BindProperty]
+        public List<string>? FormyObucheniya { get; set; }
+        [BindProperty]
+        public List<string>? Goda { get; set; }
+        [BindProperty]
+        public List<string>? Semestry { get; set; }
+
+        public string? ErrorMessage { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            _logger.LogInformation("OnPostAsync вызван");
+            _logger.LogInformation("Kurs: {Kurs}", string.Join(", ", Kurs ?? new()));
+            _logger.LogInformation("Urovni: {Urovni}", string.Join(", ", Urovni ?? new()));
+            _logger.LogInformation("FormyObucheniya: {FormyObucheniya}", string.Join(", ", FormyObucheniya ?? new()));
+            _logger.LogInformation("Goda: {Goda}", string.Join(", ", Goda ?? new()));
+            _logger.LogInformation("Semestry: {Semestry}", string.Join(", ", Semestry ?? new()));
+
+            System.Diagnostics.Debug.WriteLine(User.Identity?.Name);
+            var request = new
+            {
+                Kurs,
+                Urovni,
+                FormyObucheniya,
+                Goda,
+                Semestry
+            };
+
+            var client = _httpClientFactory.CreateClient("WithWindowsAuth");
+
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var json = JsonSerializer.Serialize(request, options);
+            _logger.LogInformation("Сформированный JSON: {Json}", json);
+            _logger.LogInformation(User.Identity?.Name);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await client.PostAsync("http://10.12.9.122:5000/Excel/download", content);
+            
+                if (response.IsSuccessStatusCode)
+                {
+                    var fileBytes = await response.Content.ReadAsByteArrayAsync();
+                    return File(fileBytes,
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        $"report_{DateTime.Now:yyyyMMdd_HHmmss}.xlsx");
+                }
+                else
+                {
+                    ErrorMessage = $"Ошибка: {response.StatusCode}";
+                    return Page();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ошибка при отправке запроса");
+                ErrorMessage = "Ошибка при подключении к API.";
+                return Page();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+namespace Uspevaemost_client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+                .AddNegotiate();
+           
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+            builder.Services.AddHttpClient("WithWindowsAuth")
+    .ConfigurePrimaryHttpMessageHandler(() =>
+        new HttpClientHandler
+        {
+            UseDefaultCredentials = true 
+        });
+
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
